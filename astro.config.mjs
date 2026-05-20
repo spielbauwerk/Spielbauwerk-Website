db0427908b675406ba5f2c6de425f3572957b433
JaccomoLorenz <jac-lor.2@web.de>
2026-05-20 16:31:52 +0200

    Configurate astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.spielbauwerk.de',
   vite: {
     plugins: [tailwindcss()]
   }
